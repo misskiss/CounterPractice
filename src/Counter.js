92669c87085ce8dd36a43d76d166c8c8e16500ff
Michelle Kiss <46352528+misskiss@users.noreply.github.com>
2019-07-17 11:54:32 -0500

    refresh
    hard refresh

---
+++ src/Counter.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// mock some data first to get a render working
+export function Counter() {
+  return (
+    <div>
+      <h1>Counter = 0</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, connect } from "react-redux";
 import { store } from "./store";
+import { Counter } from "./Counter";
 
 import "./styles.css";
 
 function App() {
-  console.log("store: ", store);
+  // console.log("store: ", store);
   return (
     <Provider store={store}>
       <div className="App">
         <h1>Hello Finn</h1>
         <h2>Start editing to see some magic happen!</h2>
+        <Counter />
       </div>
     </Provider>
   );
