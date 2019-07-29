47297920efc6e90bbac3500aa1c282e65858222c
Michelle Kiss <46352528+misskiss@users.noreply.github.com>
2019-07-29 18:40:35 -0500

    increment and decrement buttons working
    increment and decrement buttons working

--- src/Counter.js
+++ src/Counter.js
@@ -5,13 +5,29 @@ import { increment, decrement } from "./store";
 // mock some data first to get a render working
 // play around with initial state to see what renders / get it to where you want it
 export class Counter extends Component {
-  componentDidMount() {}
+  // componentDidMount() {
+  // }
+
+  handleIncrement = () => {
+    console.log("is clicked?");
+    this.props.onIncrementClick(this.props.counter);
+  };
+
+  handleDecrement = () => {
+    this.props.onDecrementClick(this.props.counter);
+  };
 
   render() {
     const counter = this.props.counter;
     return (
       <div>
         <h1>Counter = {counter}</h1>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement
+        </button>
       </div>
     );
   }
@@ -21,8 +37,15 @@ const mapState = store => {
   return { counter: store.counter };
 };
 
-const mapDispatch = () => {
-  return {};
+const mapDispatch = dispatch => {
+  return {
+    onIncrementClick: () => {
+      dispatch(increment());
+    },
+    onDecrementClick: () => {
+      dispatch(decrement());
+    }
+  };
 };
 
 export default connect(
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 
 // what should shape of store be
-const initialState = { counter: 1 };
+const initialState = { counter: 0 };
 
 // what action should happen next
 const INCREMENT = "INCREMENT";
